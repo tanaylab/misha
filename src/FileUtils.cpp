bc2d7529f14f43df5fa10a514195ac17e135f69a
John Marshall <jmarshall@hey.com>
2020-11-16 11:15:21 +0000

    FileUtils::copy_file: Add read()/write() alternative to sendfile()
    Linux's sendfile() can copy from regular file to regular file, which
    is how it is used in copy_file(). Add a read()/write() loop alternative
    for platforms without sendfile() or whose sendfile()-equivalent can only
    write to a socket, e.g., as on macOS.

--- src/FileUtils.cpp
+++ src/FileUtils.cpp
@@ -3,7 +3,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#ifdef __linux__
 #include <sys/sendfile.h>
+#endif
 #include <sys/stat.h>
 #include <sys/types.h>
 
@@ -18,6 +20,17 @@ struct FD {
     }
 };
 
+#ifndef __linux__
+struct BUFFER {
+    size_t size;
+    char *buffer;
+    BUFFER(size_t s) : size((s > 16384)? s : 16384), buffer(new char[size]) { }
+    ~BUFFER() {
+        delete [] buffer;
+    }
+};
+#endif
+
 void FileUtils::copy_file(const char *src, const char *tgt)
 {
     FD srcfd;
@@ -30,8 +43,29 @@ void FileUtils::copy_file(const char *src, const char *tgt)
         TGLError(errno, "Error trying to stat file %s: %s", src, strerror(errno));
     if ((tgtfd.fd = creat(tgt, srcstat.st_mode)) == -1)
         TGLError(errno, "Error opeining file %s for writing: %s", tgt, strerror(errno));
+#ifdef __linux__
     if (sendfile(tgtfd.fd, srcfd.fd, NULL, srcstat.st_size) == -1)
         TGLError(errno, "Error copying file %s to %s: %s\n", src, tgt, strerror(errno));
+#else
+    BUFFER buffer(srcstat.st_blksize);
+    if (buffer.buffer == NULL)
+        TGLError(errno, "Error allocating buffer: %s\n", strerror(errno));
+
+    ssize_t nread;
+    while ((nread = read(srcfd.fd, buffer.buffer, buffer.size)) > 0) {
+        const char *p = buffer.buffer;
+        size_t remaining = nread;
+        while (remaining > 0) {
+            ssize_t nwritten = write(tgtfd.fd, p, remaining);
+            if (nwritten <= 0)
+                TGLError(errno, "Error writing file %s while copying from %s: %s\n", tgt, src, strerror(errno));
+            p += nwritten;
+            remaining -= nwritten;
+        }
+    }
+    if (nread < 0)
+        TGLError(errno, "Error reading file %s while copying to %s: %s\n", src, tgt, strerror(errno));
+#endif
 }
 
 void FileUtils::move_file(const char *src, const char *tgt)
