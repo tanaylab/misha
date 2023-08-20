84d6e01f120241869a8e90e4a8e4574d93b1b745
aviezerl <aviezer.lifshitz@weizmann.ac.il>
2023-08-20 16:08:58 +0300

    Added more tests

--- tests/testthat/test-gcluster.run.R
+++ tests/testthat/test-gcluster.run.R
@@ -1,10 +1,11 @@
-{
+test_that("gcluster.run works", {
     v <- 17
     r <- gcluster.run(2 + 3, gsummary("test.fixedbin+v+qwe"), 3 + 4, 4 + 5, gsummary("test.sparse-v"), gsummary("test.rects*v"))
-    list(r[[1]]$retv, r[[2]]$retv, r[[3]]$retv, r[[4]]$retv, r[[5]]$retv, r[[6]]$retv)
-}
-{
+    expect_regression(list(r[[1]]$retv, r[[2]]$retv, r[[3]]$retv, r[[4]]$retv, r[[5]]$retv, r[[6]]$retv), "gcluster.run")
+})
+
+test_that("gcluster.run works (2)", {
     v <- 17
     r <- gcluster.run(2 + 3, gsummary("test.fixedbin+v+qwe"), 3 + 4, 4 + 5, gsummary("test.sparse-v"), gsummary("test.rects*v"), max.jobs = 2)
-    list(r[[1]]$retv, r[[2]]$retv, r[[3]]$retv, r[[4]]$retv, r[[5]]$retv, r[[6]]$retv)
-}
+    expect_regression(list(r[[1]]$retv, r[[2]]$retv, r[[3]]$retv, r[[4]]$retv, r[[5]]$retv, r[[6]]$retv), "gcluster.run.2")
+})
--- tests/testthat/test-gtrack.smooth.R
+++ tests/testthat/test-gtrack.smooth.R
@@ -1,35 +1,35 @@
-{
-    gtrack.rm("test.tmptrack", force = T)
+test_that("gtrack.smooth with test.fixedbin using LINEAR_RAMP", {
+    gtrack.rm("test.tmptrack", force = TRUE)
+    withr::defer(gtrack.rm("test.tmptrack", force = TRUE))
     gtrack.smooth("test.tmptrack", "", "test.fixedbin", 10000, alg = "LINEAR_RAMP")
     r <- gextract("test.tmptrack", gintervals(c(1, 2), 0, 1000000))
-    gtrack.rm("test.tmptrack", force = T)
-    r
-}
-{
-    gtrack.rm("test.tmptrack", force = T)
+    expect_regression(r, "gtrack.smooth.fixedbin_LINEAR_RAMP")
+})
+
+test_that("gtrack.smooth with test.fixedbin using MEAN", {
+    gtrack.rm("test.tmptrack", force = TRUE)
+    withr::defer(gtrack.rm("test.tmptrack", force = TRUE))
     gtrack.smooth("test.tmptrack", "", "test.fixedbin", 10000, alg = "MEAN")
     r <- gextract("test.tmptrack", gintervals(c(1, 2), 0, 1000000))
-    gtrack.rm("test.tmptrack", force = T)
-    r
-}
-{
-    gtrack.rm("test.tmptrack", force = T)
-    gtrack.smooth("test.tmptrack", "", "test.sparse", 10000, alg = "LINEAR_RAMP")
+    expect_regression(r, "gtrack.smooth.fixedbin_MEAN")
+})
+
+test_that("gtrack.smooth with test.sparse using LINEAR_RAMP", {
+    gtrack.rm("test.tmptrack", force = TRUE)
+    withr::defer(gtrack.rm("test.tmptrack", force = TRUE))
+    gtrack.smooth("test.tmptrack", "", "test.sparse", 10000, alg = "LINEAR_RAMP", iterator = 1000)
     r <- gextract("test.tmptrack", gintervals(c(1, 2), 0, 1000000))
-    gtrack.rm("test.tmptrack", force = T)
-    r
-}
-{
-    gtrack.rm("test.tmptrack", force = T)
-    gtrack.smooth("test.tmptrack", "", "test.array", 10000, alg = "LINEAR_RAMP")
+    expect_regression(r, "gtrack.smooth.sparse_LINEAR_RAMP")
+})
+
+test_that("gtrack.smooth with test.array using LINEAR_RAMP", {
+    gtrack.rm("test.tmptrack", force = TRUE)
+    withr::defer(gtrack.rm("test.tmptrack", force = TRUE))
+    gtrack.smooth("test.tmptrack", "", "test.array", 10000, alg = "LINEAR_RAMP", iterator = 1000)
     r <- gextract("test.tmptrack", gintervals(c(1, 2), 0, 1000000))
-    gtrack.rm("test.tmptrack", force = T)
-    r
-}
-{
-    gtrack.rm("test.tmptrack", force = T)
-    gtrack.smooth("test.tmptrack", "", "test.rects", 10000, alg = "LINEAR_RAMP")
-    r <- gextract("test.tmptrack", gintervals(c(1, 2), 0, 1000000))
-    gtrack.rm("test.tmptrack", force = T)
-    r
-}
+    expect_regression(r, "gtrack.smooth.array_LINEAR_RAMP")
+})
+
+test_that("gtrack.smooth with test.rects using LINEAR_RAMP", {
+    expect_error(gtrack.smooth("test.tmptrack", "", "test.rects", 10000, alg = "LINEAR_RAMP"))
+})
