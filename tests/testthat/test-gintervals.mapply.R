e5557a10f31f0791ecff6b5ce53b88b5ada52025
aviezerl <aviezer.lifshitz@weizmann.ac.il>
2023-08-20 16:08:58 +0300

    Added more tests

--- tests/testthat/test-gintervals.mapply.R
+++ tests/testthat/test-gintervals.mapply.R
@@ -1,7 +1,7 @@
 test_that("gintervals.mapply works", {
     expect_equal(
         gintervals.mapply(function(x) {
-            max(x + 2, na.rm = T)
+            max(x + 2, na.rm = TRUE)
         }, "test.fixedbin", gintervals(c(1, 2), 0, 100000)) %>% mutate(chrom = as.character(chrom)),
         data.frame(
             chrom = c("chr1", "chr2"),
@@ -13,20 +13,20 @@ test_that("gintervals.mapply works", {
     )
 
     expect_regression(gintervals.mapply(function(x) {
-        max(x + 2, na.rm = T)
+        max(x + 2, na.rm = TRUE)
     }, "test.fixedbin", ALLGENOME), "gintervals.mapply.fixedbin.ALLGENOME")
 
     expect_regression(gintervals.mapply(function(x) {
-        max(x + 2, na.rm = T)
+        max(x + 2, na.rm = TRUE)
     }, "test.generated_1d_1", "test.bigintervs_1d_1"), "gintervals.mapply.test.generated_1d_1.test.bigintervs_1d_1")
 })
 
 test_that("gintervals.mapply fails with 1d function for 2d intervals", {
     expect_error(gintervals.mapply(function(x) {
-        max(x + 2, na.rm = T)
+        max(x + 2, na.rm = TRUE)
     }, "test.rects", ALLGENOME, iterator = "test.rects"))
     expect_error(gintervals.mapply(function(x) {
-        max(x + 2, na.rm = T)
+        max(x + 2, na.rm = TRUE)
     }, "test.generated_2d_5", "test.bigintervs_2d_5"))
 })
 
@@ -43,10 +43,10 @@ test_that("gintervals.mapply works with intervals.set.out", {
     set.seed(60427)
     intervs <- intervs[sample(nrow(intervs)), ]
     gintervals.mapply(function(x) {
-        max(x + 2, na.rm = T)
+        max(x + 2, na.rm = TRUE)
     }, "test.fixedbin", intervs, intervals.set.out = "test.testintervs_mapply")
     expect_equal(gintervals.load("test.testintervs_mapply") %>% arrange(chrom, start, end), gintervals.mapply(function(x) {
-        max(x + 2, na.rm = T)
+        max(x + 2, na.rm = TRUE)
     }, "test.fixedbin", intervs) %>% arrange(chrom, start, end))
 })
 
@@ -57,12 +57,12 @@ test_that("gintervals.mapply works with intervals.set.out 2d", {
     set.seed(60427)
     intervs <- intervs[sample(nrow(intervs)), ]
     gintervals.mapply(function(x) {
-        max(x + 2, na.rm = T)
+        max(x + 2, na.rm = TRUE)
     }, "test.rects", intervs, iterator = c(10000, 10000), intervals.set.out = "test.testintervs_mapply")
     expect_equal(
         gintervals.load("test.testintervs_mapply") %>%
             arrange(chrom1, start1, end1, chrom2, start1, end2), gintervals.mapply(function(x) {
-            max(x + 2, na.rm = T)
+            max(x + 2, na.rm = TRUE)
         }, "test.rects", intervs, iterator = c(10000, 10000)) %>% arrange(chrom1, start1, end1, chrom2, start1, end2)
     )
 })
--- tests/testthat/test-motifs.R
+++ tests/testthat/test-motifs.R
@@ -1,12 +1,12 @@
-{
+test_that("gtrack.create_pwm_energy works", {
     allgenome <- ALLGENOME
     ALLGENOME[[1]]$end <- 1000000
     ALLGENOME[[2]]$end1 <- 1000000
     ALLGENOME[[2]]$end2 <- 1000000
-    gtrack.rm("test.tmptrack", force = T)
+    gtrack.rm("test.tmptrack", force = TRUE)
+    withr::defer(gtrack.rm("test.tmptrack", force = TRUE))
     gtrack.create_pwm_energy("test.tmptrack", "", "misha_motifs", 0, 0.02, iterator = 50)
     r <- gextract("test.tmptrack", ALLGENOME)
     ALLGENOME <- allgenome
-    gtrack.rm("test.tmptrack", force = T)
-    r
-}
+    expect_regression(r, "gtrack.create_pwm_energy")
+})
