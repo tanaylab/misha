296394433d9bab0b3cc93d57dbef18d94a2854a7
aviezerl <aviezer.lifshitz@weizmann.ac.il>
2023-08-31 14:47:14 +0300

    fixed ASAN memory alignment problems

--- src/GIntervals.cpp
+++ src/GIntervals.cpp
@@ -4,6 +4,11 @@
 
 void GIntervals::sort(bool (*cmp_function)(const GInterval &, const GInterval &))
 {
+	// if the intervals are empty do not sort 
+	if (empty()){
+		return;
+	}
+	
 	// do not sort automatically, check first that the intervals are not sorted yet
 	for (iterator iinterv = begin() + 1; iinterv < end(); ++iinterv) {
 		if (cmp_function(*iinterv, *(iinterv - 1))) {
