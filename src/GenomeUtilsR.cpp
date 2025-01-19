1535102d24e3665e4d7214f6375a917023e01f4d
aviezerl <aviezer.lifshitz@weizmann.ac.il>
2025-01-19 13:24:59 +0200

    Added `grevcomp` function (reverse complement of a DNA sequence)

---
+++ src/GenomeUtilsR.cpp
@@ -0,0 +1,33 @@
+#include <R.h>
+#include <Rinternals.h>
+#include "GenomeUtils.h"
+
+extern "C" {
+
+// R wrapper for seq2reverse_complementary
+SEXP C_revcomp(SEXP _seq) {
+    try {
+        if (!Rf_isString(_seq))
+            error("Sequence must be a character string");
+
+        SEXP ans;
+        int n = Rf_length(_seq);
+        PROTECT(ans = Rf_allocVector(STRSXP, n));
+
+        for (int i = 0; i < n; i++) {
+            std::string seq = CHAR(STRING_ELT(_seq, i));
+            std::string revcomp = seq2reverse_complementary(seq);
+            SET_STRING_ELT(ans, i, Rf_mkChar(revcomp.c_str()));
+        }
+
+        UNPROTECT(1);
+        return ans;
+    } catch(std::exception &e) {
+        error("Error in revcomp: %s", e.what());
+    } catch(...) {
+        error("Unknown error in revcomp");
+    }
+    return R_NilValue; // Never reached
+}
+
+} 
