391be59ed7e2d1efc2c651482af043bfa0e95837
aviezerl <aviezer.lifshitz@weizmann.ac.il>
2026-04-28 16:48:56 +0300

    feat(gsynth): plumb prior_mode/prior_matrix through C_gsynth_train
    C_gsynth_train accepts two new SEXP args:
    - _prior_mode    : character ('uniform', 'marginal', 'global', 'explicit')
    - _prior_matrix  : NULL or numeric n_bins x 4 matrix (column-major)
    
    Resolution happens between apply_bin_mapping and normalize_and_build_cdf,
    so 'marginal' uses post-merge counts. The result list gains 'prior'
    (n_bins x 4 numeric matrix) and 'marginal_fallbacks' (count of
    zero-observation bins that fell back to uniform).
    
    NOTE: The R-side caller in synth.R will be updated in the next commit
    to pass the two new args; until then gsynth.train() will fail.

--- src/misha-init.cpp
+++ src/misha-init.cpp
@@ -104,7 +104,7 @@ extern "C" {
     extern SEXP C_gseq_pwm_multitask(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
     extern SEXP C_gseq_pwm_edits(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
     extern SEXP C_gseq_kmer(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
-    extern SEXP C_gsynth_train(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
+    extern SEXP C_gsynth_train(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
     extern SEXP C_gsynth_sample(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
     extern SEXP C_gsynth_replace_kmer(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
     extern SEXP C_gsynth_score(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP,
@@ -211,7 +211,7 @@ static const R_CallMethodDef CallEntries[] = {
     {"C_gseq_pwm_multitask", (DL_FUNC)&C_gseq_pwm_multitask, 17},
     {"C_gseq_pwm_edits", (DL_FUNC)&C_gseq_pwm_edits, 14},
     {"C_gseq_kmer", (DL_FUNC)&C_gseq_kmer, 9},
-    {"C_gsynth_train", (DL_FUNC)&C_gsynth_train, 12},
+    {"C_gsynth_train", (DL_FUNC)&C_gsynth_train, 14},
     {"C_gsynth_sample", (DL_FUNC)&C_gsynth_sample, 13},
     {"C_gsynth_replace_kmer", (DL_FUNC)&C_gsynth_replace_kmer, 6},
     {"C_gsynth_score", (DL_FUNC)&C_gsynth_score, 14},
